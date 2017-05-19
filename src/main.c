4f7c335c7902357f5fb69e9b1984a3cc5d0a4ffc
Minh Nhuong <proton207@gmail.com>
2017-05-19 21:09:23 +0700

    final. the best

--- src/main.c
+++ src/main.c
@@ -19,14 +19,9 @@
 #include "utils/uartstdio.h"
 
 
-COMPLEX 	CentralBuffer[M][N] ;		
-bool 					FlagIn[M] ;
-bool 					FlagOut[M] ;
-uint8_t 			Incount = 0; 
-uint8_t 			Outcount = M - 3 ;
-
+COMPLEX 	CentralBuffer[N] ;		
+bool 					FlagIn  = true;
 uint32_t 		ErrCount_Adc  =0  ;
-uint32_t 		ErrorCount_uart = 0 ;
 extern void PF4_IntHandler(void);
 
 void GPIOF_config(void)
@@ -59,7 +54,6 @@ void uDMA_config(void)
   IntEnable(INT_UDMAERR);
 	
 	uDMAControlBaseSet(ui8ControlTable);		//set base addr for control table
-	//uDMAChannelAssign(UDMA_CH14_ADC0_0); 			
 	
 	IntEnable(INT_UDMA);
 																	
@@ -71,42 +65,25 @@ void ADC_config(void)
 {				//??
 	
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-	//	SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_ADC0);
-				//select ADC pin PE3
 	GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
-	
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);				
-			//configure ADC, sequencer SS3( 1 sample)
 	SysCtlDelay( 20);
 	ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_TIMER, 0);			
-			//configure ADC SS3, step 0, single input channel 0
-
 	ADCSequenceStepConfigure(ADC0_BASE, 3 , 0, ADC_CTL_CH0 | ADC_CTL_END | ADC_CTL_IE);										
-	//enable uDMA for ADC SS0
 	ADCSequenceDMAEnable(ADC0_BASE,3);
-
 	ADCIntEnableEx(ADC0_BASE, ADC_INT_DMA_SS3 ) ;
-	//enable ADC active during CPU sleeping
-
 	IntEnable(INT_ADC0SS3) ;
-	
 	uDMAChannelAttributeDisable(UDMA_CHANNEL_ADC3,UDMA_ATTR_ALTSELECT | 
 																	UDMA_ATTR_REQMASK | UDMA_ATTR_HIGH_PRIORITY | UDMA_ATTR_USEBURST) ;
-	
-	// need to be confirmed. change DMA priority every 1/25k (s), so use bust request or not
- //	uDMAChannelAttributeEnable(UDMA_CHANNEL_ADC0, UDMA_ATTR_USEBURST ) ; // use bust request 
 	uDMAChannelControlSet(UDMA_CHANNEL_ADC3 | UDMA_PRI_SELECT, 
 							UDMA_SIZE_32 | UDMA_SRC_INC_NONE | UDMA_DST_INC_32 | UDMA_ARB_1 );
 	uDMAChannelControlSet(UDMA_CHANNEL_ADC3 | UDMA_ALT_SELECT, 
 							UDMA_SIZE_32 | UDMA_SRC_INC_NONE | UDMA_DST_INC_32 | UDMA_ARB_1 );
-							// hope this shit will work :((
 		uDMAChannelTransferSet(UDMA_CHANNEL_ADC3 | UDMA_PRI_SELECT, UDMA_MODE_PINGPONG,
 		(void *)(ADC0_BASE + ADC_O_SSFIFO3 ) , ui32ADCBuffA,  sizeof(ui32ADCBuffA)) ;
 		uDMAChannelTransferSet(UDMA_CHANNEL_ADC3 | UDMA_ALT_SELECT, UDMA_MODE_PINGPONG,
 		(void *)(ADC0_BASE + ADC_O_SSFIFO3 ), ui32ADCBuffB, sizeof(ui32ADCBuffB)) ;
-		
 	uDMAChannelEnable(UDMA_CHANNEL_ADC3) ;	
-	//start when trigger is detected
 	 ADCIntClear(ADC0_BASE, 3);
 	ADCSequenceEnable(ADC0_BASE, 3);																													
 	
@@ -120,124 +97,21 @@ void Timer_config(void)
 	TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet() / FS-1 ) ;			// trigger ADC at FS (kHz)
 	TimerControlTrigger(TIMER0_BASE, TIMER_A, true );
 	SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_TIMER0);
-	//IntMasterEnable() ;
-	
-	//TimerEnable(TIMER0_BASE, TIMER_A );		// put here or main
 }
 
 void UART_config(void)
 {
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-	SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_UART0);
-	GPIOPinConfigure(GPIO_PA0_U0RX);
-	GPIOPinConfigure(GPIO_PA1_U0TX);
-	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0|GPIO_PIN_1);
-	
-	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-		(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));			//1.2M baud
-	
-	UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX4_8, UART_FIFO_RX4_8);
-	UARTEnable(UART0_BASE) ;
-	UARTDMAEnable(UART0_BASE, UART_DMA_TX);
-	
-	IntEnable(INT_UART0);										//interrupt UART0
-	
-	uDMAChannelAttributeDisable(UDMA_CHANNEL_UART0TX, UDMA_ATTR_ALTSELECT | 
-										UDMA_ATTR_HIGH_PRIORITY | UDMA_ATTR_REQMASK | UDMA_ATTR_USEBURST) ;
-	//uDMAChannelAttributeEnable(UDMA_CHANNEL_UART0TX, UDMA_ATTR_USEBURST );		// use bust request
-	//				NEED TO CONFIRM: USEBURST OR NOT
-	//
-	uDMAChannelControlSet(UDMA_CHANNEL_UART0TX | UDMA_PRI_SELECT, 
-							   UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE | UDMA_ARB_4);
-	uDMAChannelControlSet(UDMA_CHANNEL_UART0TX | UDMA_ALT_SELECT , 
-									UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE | UDMA_ARB_4 ) ;
-	
-	uDMAChannelTransferSet(UDMA_CHANNEL_UART0TX | UDMA_PRI_SELECT, UDMA_MODE_PINGPONG,
-										ui8TxBuffA, (void *)(UART0_BASE + UART_O_DR),sizeof(ui8TxBuffA)) ;
-	uDMAChannelTransferSet(UDMA_CHANNEL_UART0TX | UDMA_ALT_SELECT, UDMA_MODE_PINGPONG,
-										ui8TxBuffB, (void *)(UART0_BASE + UART_O_DR ), sizeof(ui8TxBuffB)) ;
-	
 
-	uDMAChannelEnable(UDMA_CHANNEL_UART0TX) ; //just enable transmitter, not receive anyway
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_UART0);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    UARTStdioConfig(0, 115200, 16000000);
 }
 
-void UART0_IntHandler(void)
-{
-	uint32_t ui32Status ;
-	uint32_t ui32Mode ;
-	uint16_t  n ;
-
-	
-	ui32Status = UARTIntStatus(UART0_BASE, true) ;
-	UARTIntClear(UART0_BASE, ui32Status) ;
-	
-	ui32Mode =  uDMAChannelModeGet(UDMA_CHANNEL_UART0TX | UDMA_PRI_SELECT);
-	if(ui32Mode ==UDMA_MODE_STOP)
-	{
-		// transfer buffer A is done ???
-		//
-		//		Put something here to notify CPU send data to buffer A
-
-			if(FlagOut[Outcount] == true)
-				{
-					for(n=0;n<N;n++)
-					{
-				ui8TxBuffA[n] =(uint8_t)  (CentralBuffer[Outcount][n]).real ;
-					}
-					Outcount++ ;
-					if(Outcount==M)					//update counter
-						Outcount = 0 ;
-					
-					FlagOut[Outcount] = false ;
-					FlagIn[Outcount] = true ;
-				}
-				else ErrorCount_uart++ ;
-		
-					// configure to re-use this buffer when DMA playing with buffer B	
-		uDMAChannelTransferSet(UDMA_CHANNEL_UART0TX | UDMA_PRI_SELECT,
-		UDMA_MODE_PINGPONG, ui8TxBuffA,(void *)( UART0_BASE + UART_O_DR ),sizeof(ui8TxBuffA)) ;
-				
-	}
-	
-	ui32Mode = uDMAChannelModeGet(UDMA_CHANNEL_UART0TX | UDMA_ALT_SELECT );
-	
-	if(ui32Mode == UDMA_MODE_STOP) 
-	{
-		//Transfer buffer B is done ??
-		//
-		//	Put something here to notify CPU send data to buffer B
-		//
-		
-					if(FlagOut[Outcount] == true)
-				{
-					for(n=0;n<N;n++)
-					{
-				ui8TxBuffB[n] =(uint8_t)  (CentralBuffer[Outcount][n]).real ;
-					}
-					Outcount++ ;
-					if(Outcount==M)					//update counter
-					Outcount = 0 ;
-					
-					FlagOut[Outcount] = false ;
-					FlagIn[Outcount] = true ;
-				}
-				else ErrorCount_uart++ ;
-		
-		uDMAChannelTransferSet(UDMA_CHANNEL_UART0TX | UDMA_ALT_SELECT, 
-		 UDMA_MODE_PINGPONG, ui8TxBuffB, (void *)(UART0_BASE + UART_O_DR ), sizeof(ui8TxBuffB)) ;
-		
-	}
-	
-	if(!uDMAChannelIsEnabled(UDMA_CHANNEL_ADC0) )				//if DMA ADC0 is disabled?
-	{
-		//
-		// Put something here to notify CPU or Timer know that DMA ADC0 is disabled
-		// should re-configure DMA for ADC0 transfer
-	}
-	
-	
-}
 
 void uDMA_Err_Handler(void)
 {
@@ -262,44 +136,31 @@ void ADC0_IntHandler(void)
 		if(ui32Mode == UDMA_MODE_STOP )
 		{	
 
-		  if(FlagIn[Incount] == true)
+		  if(FlagIn == true)
 			{
 					for(n=0;n<N;n++)
 					{
-							(CentralBuffer[Incount][n]).real = (float) (ui32ADCBuffA[n])/4096 ;
+							(CentralBuffer[n]).real = (float) (ui32ADCBuffA[n])/20 ;
 					}
-					FlagIn[Incount] = false ;
-					FlagOut[Incount] = false ;
-					Incount++;
-					if(Incount==M)
-							Incount = 0 ;
+					FlagIn = false ;
 			}
 			else ErrCount_Adc++ ;
 		
 			uDMAChannelTransferSet(UDMA_CHANNEL_ADC3 | UDMA_PRI_SELECT, UDMA_MODE_PINGPONG,
 												(void *)( ADC0_BASE + ADC_O_SSFIFO3), ui32ADCBuffA, sizeof(ui32ADCBuffA)) ;
 			
-			
 		}
 	ui32Mode = uDMAChannelModeGet(UDMA_CHANNEL_ADC3 | UDMA_ALT_SELECT) ;
 		if(ui32Mode == UDMA_MODE_STOP )
 		{
-			//
-			// notify CPU that buffer B (ADC) is done => copy it into circullar buffer
-			//
-//			if((currentMode == fftMode)||(currentMode == phaseMode)) 
 
-				if(FlagIn[Incount] == true)
+				if(FlagIn== true)
 				{
 					for(n=0;n<N;n++)
 					{
-						(CentralBuffer[Incount][n]).real = (float) (ui32ADCBuffB[n])/4096 ;
+						(CentralBuffer[n]).real = (float) (ui32ADCBuffB[n])/20 ;
 					}
-					FlagIn[Incount] = false ;
-					FlagOut[Incount] = false ;
-					Incount++ ;
-					if(Incount==M)
-						Incount =0 ;
+					FlagIn= false ;
 				}
 				else ErrCount_Adc++ ;
 			
@@ -314,16 +175,12 @@ extern void PF4_IntHandler( void)
 	while(!GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0));
 	switch(currentMode)
 	{
-		case IdleMode 			: 
-		{			currentMode = fftMode; 	
-					GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 , GPIO_PIN_1);
-		}	break ;
 		case fftMode		:  
 		{			currentMode = osciMode  ; 
 					GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
 		}	break ;
 		case osciMode  :  
-		{			currentMode = IdleMode   ;
+		{			currentMode = fftMode   ;
 					GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
 		}	break ;			
 	} 
@@ -336,13 +193,11 @@ int main (void)
 {
 	
   uint32_t 		n;
-	uint8_t 			count ;
-	float tempp  = 1.2345 ;
-//	float fft_debug[N] ;
-	SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);		//80MHz
-	SysCtlPeripheralClockGating(true) ;			// enable peripherals to operate when CPU is in sleep
+	uint8_t 			transfer8 ;
+	SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);	
+	SysCtlPeripheralClockGating(true) ;			
 	
-	FPULazyStackingEnable();									//enable floating point 
+	FPULazyStackingEnable();									
   FPUEnable();
   
 	GPIOF_config();
@@ -350,65 +205,48 @@ int main (void)
 	UART_config();
 	Timer_config() ;
 	ADC_config() ;
-/*
-	for(n=0;n<N;n++)
-	{
-		(CentralBuffer[5][n]).real = sinf(2*PI*5*n/N) ;
-		(CentralBuffer[5][n]).imag = 0 ;
-	}
-	*/
+
 	 twiddle_array(N, twiddle) ;
-	
-		for(count = 0; count<M;	count++)
-		{
-			FlagIn[count] = true ;
-			FlagOut[count] = true ;
-		}
 	
 		IntMasterEnable() ;
 	TimerEnable(TIMER0_BASE, TIMER_A ) ; // begin
 	  while(1)
   {
-		while(currentMode == IdleMode)
-		{
-			
-		}
 		while(currentMode == fftMode)
 		{
-
-			for(count=0; count<M;count++)
-			{
-				if(FlagIn[count] == false)
+				if(FlagIn == false)
 				{
-					FlagOut[count] = false ;
-					FlagIn[count]  = false ;		//make sure that no anytask can touch this memory
-					fft(CentralBuffer[count] , N , twiddle) ;
+					fft(CentralBuffer , N , twiddle) ;
 					for(n = 0; n< N ; n++)
 					{
-					(CentralBuffer[count][n]).real = sqrtf((CentralBuffer[count][n]).real * (CentralBuffer[count][n]).real + (CentralBuffer[count][n].imag)*(CentralBuffer[count][n]).imag) ;
-
+					(CentralBuffer[n]).real = sqrtf((CentralBuffer[n]).real * (CentralBuffer[n]).real + (CentralBuffer[n].imag)*(CentralBuffer[n]).imag) ;
 					}
-					FlagOut[count] = true ;
 					
-					//FlagIn[count] = true ;
-					
+					UARTCharPut(UART0_BASE, 255) ;
+					for(n=0;n<N;n++)
+					{
+						transfer8 = (uint8_t) CentralBuffer[n].real ;
+						if (transfer8 ==255)
+							transfer8 = 250 ;
+						UARTCharPut(UART0_BASE, transfer8) ;
+					}
+					FlagIn = true ;
 				}
-
 			}
-		}
 		while(currentMode == osciMode)
 		{
-			for(count= 0; count<M ; count++)
-			{
-				if(FlagIn[count]==false)
+				if(FlagIn==false)
 				{
-					FlagOut[count] = true ;
+					for(n=0;n<N;n++)
+					{
+						transfer8 = (uint8_t) CentralBuffer[n].real ;
+						if (transfer8 ==255)
+							transfer8 = 250 ;
+						UARTCharPut(UART0_BASE, transfer8) ;
 				}
-			}
-			
+				FlagIn = true ;
 		}
 
   }
-
 }
-
+}
